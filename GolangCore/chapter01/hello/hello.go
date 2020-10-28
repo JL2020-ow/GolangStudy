f82a614d00c0ab90f32bbaf25bd24b94ddd6a9c0
JL2020-ow <williamalex22@126.com>
2020-10-28 22:46:26 +0800

    first commit

--- GolangCore/chapter01/hello/hello.go
+++ GolangCore/chapter01/hello/hello.go
@@ -4,4 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Printf("Hello,world.你好，世界！\n")
+	fmt.Println("hello")
+	fmt.Println("hh")
+
 }
---
+++ GolangCore/chapter01/pointer/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("golang指针学习")
+}
